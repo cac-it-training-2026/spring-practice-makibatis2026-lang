0204c25835bd186ca9597d3b98471cf64bbd9fd1
m-akiba <m-akiba@CD26301L.arksystems.co.jp>
2026-06-01 17:31:24 +0900

    2026/06/01(月)：practice.p05.q06まで終了しました

--- spring_practice/src/main/java/jp/co/sss/practice/p06/entity/Area.java
+++ spring_practice/src/main/java/jp/co/sss/practice/p06/entity/Area.java
@@ -1,5 +1,24 @@
 package jp.co.sss.practice.p06.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "area")
 public class Area {
 
+	@Id
+	@GeneratedValue()
+	@SequenceGenerator()
+	private Integer areaId;
+	
+	@Column
+	private String areaName;
+	
+	@Column
+	private String detail;
 }
